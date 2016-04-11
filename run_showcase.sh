92bb367653287a8c3f6db1791c2befd0615ebb15
JosefSommerauer <jsommerauer@gmail.com>
2016-04-11 17:55:12 +0000

    Update run_showcase.sh

--- run_showcase.sh
+++ run_showcase.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-su -c /home/ubuntu/tdot/speed.sh root
-/home/ubuntu/tdot/disable_usb_autosuspend.sh
-/home/ubuntu/tdot/camera.sh
-cd /home/ubuntu/tdot/
-/home/ubuntu/tdot/tdot&
-gnome-terminal -e /home/ubuntu/tdot/info.sh
+chmod +x speed.sh
+chmod +x disable_usb_autosuspend.sh
+chmod +x camera.sh
+
+su -c speed.sh root
+./disable_usb_autosuspend.sh
+./camera.sh
+./tdot&
+gnome-terminal -e ./info.sh
